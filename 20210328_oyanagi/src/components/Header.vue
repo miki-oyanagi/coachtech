0096b3bc2937aa595c29d3792f7dff4a43c3b639
miki oyanagi <mikioyanagi.0806@icloud.com>
2021-03-28 16:22:13 +0900

    first-commit

---
+++ 20210328_oyanagi/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <p>住 所 検 索</p>
+  </header>
+</template>
+
+<style scoped>
+header{
+  width: 100%;
+  background: goldenrod;
+  text-align: center;
+  height: 50%;
+}
+
+header p{
+  color: black;
+  font-size: 36px;
+}
+</style>
