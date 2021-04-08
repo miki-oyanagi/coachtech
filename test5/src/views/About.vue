af459be458f48ea52a32a0064be9791495739c09
miki oyanagi <mikioyanagi.0806@icloud.com>
2021-04-08 22:09:54 +0900

    first-commit

---
+++ test5/src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>※homeを押して戻ってください</h1>
+</template>
+
+<style scoped>
+h1{
+  font-size: 16px;
+}
+</style>
---
+++ test5/src/views/Member.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>{{id}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props:["id"]
+};
+</script>
